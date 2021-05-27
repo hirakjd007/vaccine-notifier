1c016dd60004b241bed9b4d92d1c77f5a60a88b8
hirakjd007 <43433351+hirakjd007@users.noreply.github.com>
2021-05-27 15:13:17 +0530

    Update covaccinate.py

--- covaccinate.py
+++ covaccinate.py
@@ -1,7 +1,6 @@
-import requests
 import time
 import json
-from sys import exit
+import requests
 
 with open('config.json') as data:
     filter_data = json.load(data)
@@ -14,16 +13,16 @@ while True:
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
         print('Executing script !!!')
         response = session.get(url, headers=headers)
-        message_string = ""
+        MESSAGE = ""
         if response.ok:
             response = response.json()
             for center in response['centers']:
                 for session in center['sessions']:
 
                     if (session['min_age_limit'] == filter_data['min_age_limit'] and session['available_capacity'] > filter_data['available_capacity'] and session['vaccine'] == filter_data['vaccine']):
-                        message_string += f"<b>{today}- VACCINE AVAILABLE FOR {filter_data['pincode']} of {session['min_age_limit']} age group!!</b> \nAvailable- {session['available_capacity']} slots \nCenter- {center['name']},{center['block_name']}\nDate- {session['date']}\nAge- {session['min_age_limit']} + \nVaccine:<strong>{session['vaccine']}</strong>  \nDose 1: {session['available_capacity_dose1']}Dose 2: {session['available_capacity_dose2']}  \n <a href='https://selfregistration.cowin.gov.in/'>Book on CoWin</a>\n\n"
-            if(message_string != ""):
-                url = f"https://api.telegram.org/{filter_data['my_token']}/sendMessage?chat_id={filter_data['chat_id']}&text={message_string}&parse_mode=HTML"
+                        MESSAGE += f"<b>{today}- VACCINE AVAILABLE FOR {filter_data['pincode']} of {session['min_age_limit']} age group!!</b> \nAvailable- {session['available_capacity']} slots \nCenter- {center['name']},{center['block_name']}\nDate- {session['date']}\nAge- {session['min_age_limit']} + \nVaccine:<strong>{session['vaccine']}</strong>  \nDose 1: {session['available_capacity_dose1']}Dose 2: {session['available_capacity_dose2']}  \n <a href='https://selfregistration.cowin.gov.in/'>Book on CoWin</a>\n\n"
+            if(MESSAGE != ""):
+                url = f"https://api.telegram.org/{filter_data['my_token']}/sendMessage?chat_id={filter_data['chat_id']}&text={MESSAGE}&parse_mode=HTML"
                 requests.post(url)
                 exit()
         time.sleep(filter_data['poll_interval'])
